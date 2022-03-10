0898aa3562928d58d45132c0b59f10587bee0c9e
jennacohen <54635515+jennacohen@users.noreply.github.com>
2022-03-10 16:52:11 -0500

    typeit update

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 // console.log("Hello world!")
 
-new TypeIt("#myElement", {
-    strings: "journalist + designer",
-}).go();
+new TypeIt("#myElement")
+    .type("i'm a digital designer")
+    .pause(1500)
+    .delete(16)
+    .pause(1500)
+    .type("storyteller")
+    .pause(1500)
+    .delete(11)
+    .pause(1500)
+    .type("storyteller")
+
+    .go();
+
+
+
+//     getBeforeInit={(instance) => {
+//     instance.type("Hi, I'm Alxe").pause(750).delete(2).pause(500).type("ex!");
+
+//     // Remember to return it!
+//     return instance;
+//     // strings: "journalist + designer",
+// }).go();

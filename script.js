6c49917719c322a9bc149a4b594fe5bea4fc3c87
Jenna Cohen <jennaco@terpmail.umd.edu>
2024-09-27 12:47:57 -0400

    update

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
 // console.log("Hello world!")
 
-// slow deletion speed 
+// slow deletion speed
 // https://www.typeitjs.com/docs/vanilla/usage#custom-properties
 
 new TypeIt("#myElement", {
-    deleteSpeed: 70,
+  deleteSpeed: 70,
 })
 
-    .pause(500)
-    .type("i'm a digital designer")
-    .pause(1400)
-    .delete(16)
-    .pause(800)
-    .type("ux enthusiast")
-    .pause(1400)
-    .delete(13)
-    .pause(800)
-    .type("storyteller.")
+  .pause(500)
+  .type("I'm a digital creative")
+  .pause(1400)
+  .delete(16)
+  .pause(800)
+  .type("visual journalist")
+  .pause(1400)
+  .delete(17)
+  .pause(800)
+  .type("storyteller.")
 
-    .go();
-
-
+  .go();
 
 //     getBeforeInit={(instance) => {
 //     instance.type("Hi, I'm Alxe").pause(750).delete(2).pause(500).type("ex!");
@@ -30,28 +28,27 @@ new TypeIt("#myElement", {
 //     // strings: "journalist + designer",
 // }).go();
 
-
 $(document).ready(function () {
-    var elem = $("body");
-    var color = elem.css('background-color')
-    var counter = 0
-    $("#btn").click(function () {
-        counter = counter + 1
-        if (counter % 2 == 0) {
-            $("body").css({ "backgroundColor": "#120902" });
-            $(".toggle-color").css({ "color": "#FFFFF0" });
-            $(".image-border-color").css({ "border-color": "#FFFFF0" });
-            $(".footer-color").css({ "background-color": "#FFFFF0" });
-            $(".footer-text-color").css({ "color": "#120902" });
-        } else {
-            $("body").css({ "backgroundColor": "#FFFFF0" });
-            $(".toggle-color").css({ "color": "#120902" });
-            $(".image-border-color").css({ "border-color": "#120902" });
-            $(".footer-color").css({ "background-color": "#120902" });
-            $(".footer-text-color").css({ "color": "#FFFFF0" });
-        }
-        // console.log(color)
+  var elem = $("body");
+  var color = elem.css("background-color");
+  var counter = 0;
+  $("#btn").click(function () {
+    counter = counter + 1;
+    if (counter % 2 == 0) {
+      $("body").css({ backgroundColor: "#120902" });
+      $(".toggle-color").css({ color: "#FFFFF0" });
+      $(".image-border-color").css({ "border-color": "#FFFFF0" });
+      $(".footer-color").css({ "background-color": "#FFFFF0" });
+      $(".footer-text-color").css({ color: "#120902" });
+    } else {
+      $("body").css({ backgroundColor: "#FFFFF0" });
+      $(".toggle-color").css({ color: "#120902" });
+      $(".image-border-color").css({ "border-color": "#120902" });
+      $(".footer-color").css({ "background-color": "#120902" });
+      $(".footer-text-color").css({ color: "#FFFFF0" });
+    }
+    // console.log(color)
 
-        // $("body").css({ "maxWidth": "white" });
-    });
+    // $("body").css({ "maxWidth": "white" });
+  });
 });
